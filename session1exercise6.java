82bfda8e064dbf5ae83c6bd72d3725b8738f0440
PhamthihuyenTrang2208 <huyentrang22ori@gmail.com>
2025-11-19 09:46:05 +0900

    Add files via upload

---
+++ session1exercise6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ public class session1exercise6{
+    public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+        while(true){
+            float width;
+            float height;
+            do {System.out.println("Hãy nhập vào chiều rộng của hình chữ nhật");
+            width = sc.nextFloat();
+        if(width<0) System.out.println("Hãy nhập vào chiều rộng hợp lệ, phải lớn hơn 0 ");
+        } while(width<0);
+          do {System.out.println("Hãy nhập vào chiều dài của hình chữ nhật");
+             height= sc.nextFloat();
+             if(height<0) System.out.println("Hãy nhập vào chiều dài hợp lệ, phải lớn hơn 0");
+             }while(height<0);
+    float area= width*height;
+      float  circumference= (width+height)*2;
+        System.out.printf("Diện tích của hình chữ nhật có chiều dài %f và chiều rộng %f là : %.2f %n",height,width,area);
+        System.out.printf("Chu vi của hình chữ nhật có chiều dài %f và chiều rộng %f là : %.2f%n",height,width,circumference);
+ }}}
